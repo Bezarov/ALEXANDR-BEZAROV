71243e39e78f6c26e7df4d3ef57f389c7ee29938
Alexandr <bezarov.sasha@gmail.com>
2023-03-17 12:50:06 +0200

    Homework-1 i fink done

---
+++ src/main/java/homework_nr_1/HomeWork1.java
@@ -0,0 +1,26 @@
+package homework_nr_1;
+
+public class HomeWork1 {
+    public static void main(String[] args){
+    long CurrentWorldPopulation = 7811080815L;
+    int CarsProducedThisYear = 55235358;
+    char FirstLetterInAlphabet = 'A';
+    boolean JavaIsAmazing = true;
+    float Pi = 3.14159F;
+    double Price = 1003.1234123;
+    double Tax = 1112873612.1234568;
+    int Quantity = 55235358;
+    double Sum = 0;
+    double Total = Price + Tax * Quantity + Sum;
+    //Супер мудрённый коммент
+        /*
+        * Супер
+        * мудрённый
+        * многострочный
+        * оммент
+         */
+        System.out.println("Hello World!!" + "\n" + CurrentWorldPopulation + "\n" +
+            CarsProducedThisYear + "\n" + FirstLetterInAlphabet + "\n" +
+                JavaIsAmazing + "\n" + Pi + "\n" + Total); // => Супер мудрённый вывод)
+    }
+}
