510006983d5864cfa92f9b31c7fe5f5a7dcf6d8b
Alexandr <bezarov.sasha@gmail.com>
2023-04-05 17:58:38 +0300

    homework_nr_4_additional added validation

--- src/main/java/homework_nr_4_additional/JavaClock.java
+++ src/main/java/homework_nr_4_additional/JavaClock.java
@@ -10,9 +10,9 @@ public class JavaClock {
         seconds = 0;
     }
     public JavaClock(int hours, int minuts, int seconds) {
-        this.hours = hours;
-        this.minuts = minuts;
-        this.seconds = seconds;
+        setHours(hours);
+        setMinuts(minuts);
+        setSeconds(seconds);
     }
     public JavaClock (int secondsSinceMidnight) {
         seconds = secondsSinceMidnight;
@@ -26,9 +26,8 @@ public class JavaClock {
             hours = hours / 60;
             System.out.println("Сконвертировано из секунд " + secondsSinceMidnight + " в: "
                     + hours + ":" + minuts + ":" + seconds);
-            return 1;
         }
-        else return 1;
+        return 1;
     }
     //Метод который разбирает время из часового представления в секунды
     public int unSetClock(JavaClock secondClock){
@@ -43,15 +42,29 @@ public class JavaClock {
     public int getSeconds(){
         return seconds;
     }
-    public void setHours(int hours) { this.hours = hours; }
+    public void setHours(int hours) {
+        if (hours < 24)
+            this.hours = hours;
+        else this.hours = hours / 24;
+    }
     public void setMinuts(int minuts){
-        this.minuts = minuts;
+        if (minuts < 60)
+             this.minuts = minuts;
+        else this.hours += minuts / 60;
     }
     public void setSeconds(int seconds){
-        this.seconds = seconds;
+        if (seconds < 60)
+             this.seconds = seconds;
+        else this.minuts += seconds / 60;
     }
-    public void tickUp(){ this.seconds += 1; }
-    public void tickDown(){ this.seconds -= 1; }
+    public void tickUp(){
+        setSeconds(this.seconds += 1); }
+    public void tickDown(){
+        if (this.seconds <= 0){
+            this.minuts -= 1;
+            this.seconds += 59;
+            this.seconds -= 1;
+        } else this.seconds -= 1; }
     //Метод который добавляет время двух обьектов и выводит сумму в секундах
     public int addClock(JavaClock secondClock, JavaClock firstClock){
          return unSetClock(secondClock) + unSetClock(firstClock);
