c8a36438e0005ed9cd69ecee7b6689073e3c41da
Alexandr <bezarov.sasha@gmail.com>
2023-05-17 17:43:04 +0300

    homework_nr_13

---
+++ src/main/java/homework_nr_14/GenericsClass.java
@@ -0,0 +1,28 @@
+package homework_nr_14;
+
+import homework_nr_13.Numbers;
+
+import java.io.Serializable;
+
+public class GenericsClass<T, K, V> {
+    T t;
+    V v;
+    K k;
+    public GenericsClass(T t, V v, K k) {
+        this.t = t;
+        this.v = v;
+        this.k = k;
+    }
+    public T getT() {
+        return t;
+    }
+    public V getV() {
+        return v;
+    }
+    public K getK() {
+        return k;
+    }
+    public static <T extends Comparable<T>, V extends Animal & Serializable, K extends Numbers> String getNamesOfClasses(T t, V v, K k){
+        return t.getClass().getSimpleName() + v.getClass().getSimpleName() + k.getClass().getSimpleName();
+    }
+}
