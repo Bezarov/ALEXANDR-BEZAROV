ec2f82cb148aac9d48b82a8057a419347042a26c
Alexandr <bezarov.sasha@gmail.com>
2023-06-04 14:58:24 +0300

    homeWork_15_still_not_done

--- src/main/java/homework_nr_15/Main.java
+++ src/main/java/homework_nr_15/Main.java
@@ -1,10 +1,11 @@
 package homework_nr_15;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 5; i++) {
             new ThreadGenerator().start();
         }
-
 
         Thread t5 = new Thread(new ThreadGenerator1());
         Thread t6 = new Thread(new ThreadGenerator1());
@@ -13,74 +14,88 @@ public class Main {
         t5.join();
         t6.join();
 
-        Producer t7 = new Producer();
-        Consumer t8 = new Consumer();
-        t7.start();
-        t8.start();
-
+//        ArrayBlockingQueue<Integer> synArray = new ArrayBlockingQueue<>(1, true);
 //        new Thread(() -> {
-//            synchronized (threadGenerator2.sharedNumber) {
-//                synchronized (threadGenerator2.stop) {
-//                    while (threadGenerator2.sharedNumber <= 500000 && !threadGenerator2.stop) {
-//                        System.out.println(threadGenerator2.sharedNumber + " " + Thread.currentThread().getName());
-//                        Thread.currentThread().notify();
-//                        try {
-//                            Thread.currentThread().wait();
-//                        } catch (InterruptedException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//                    }
+//            for(int i = 0; i < 100000; i++) {
+//                try {
+//                    synArray.put(i);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
 //                }
 //            }
-//            threadGenerator2.stop = true;
-//            System.out.println("Stopped " + threadGenerator2.sharedNumber + " " + Thread.currentThread().getName());
 //        }).start();
-    }
+//        new Thread(() -> {
+//            while (synArray.peek() < 50000) {
+//                try {
+//                    System.out.println(synArray.take() + " " + Thread.currentThread().getName());
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }).start();
 
-    static class ThreadGenerator extends Thread {
-        @Override
-        public void run() {
-            System.out.println("Hello World " + getName());
-        }
-    }
 
-    static class ThreadGenerator1 implements Runnable {
-        private static volatile int sharedNumber = 0;
-        @Override
-        public void run() {
-            while (sharedNumber < 1000000)
-                sharedNumber++;
-            System.out.println(sharedNumber + " " + Thread.currentThread().getName());
+        Thread t7 = new Thread(new Runnable1());
+        t7.start();
+        Thread t8 = new Thread(new Runnable2());
+        t8.start();
+    }
+}
+
+class ThreadGenerator extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Hello World " + getName());
+    }
+}
+
+class ThreadGenerator1 implements Runnable {
+    private static volatile int sharedNumber = 0;
+
+    @Override
+    public void run() {
+        while (sharedNumber < 1000000)
+            sharedNumber++;
+        System.out.println(sharedNumber + " " + Thread.currentThread().getName());
+    }
+}
+
+class SynArray {
+    ArrayBlockingQueue<Integer> synArray = new ArrayBlockingQueue<>(2);
+    boolean stop = false;
+    public void producer() throws InterruptedException {
+        for (int i = 1; i < 100000 || !stop; i++) {
+            synArray.put(i);
         }
     }
-    static class Producer extends Thread {
-        public static volatile Integer sharedNumber = 0;
-        public static volatile Boolean stop = false;
-        @Override
-        public void run() {
-            while (sharedNumber <= 1000000 && !stop) {
-                synchronized (sharedNumber) {
-                    sharedNumber++;
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
+    public void consumer() throws InterruptedException {
+        while (!stop) {
+            int arrayNumber = synArray.take();
+            if (arrayNumber > 50000) {
+                stop = true;
+                System.out.println("Stopped - number reached: ");
             }
+            System.out.println(arrayNumber + " " + Thread.currentThread().getName());
         }
     }
-    static class Consumer extends Thread {
-        @Override
-        public void run() {
-            synchronized (Producer.sharedNumber) {
-                while (Producer.sharedNumber <= 500000) {
-                    System.out.println(Producer.sharedNumber + " " + Thread.currentThread().getName());
-                    notifyAll();
-                }
-                Producer.stop = true;
-                System.out.println("Stopped " + Producer.sharedNumber + " " + Thread.currentThread().getName());
-            }
+}
+class Runnable1 extends SynArray implements Runnable {
+    @Override
+    public void run() {
+        try {
+            producer();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+class Runnable2 extends SynArray implements Runnable{
+    @Override
+    public void run() {
+        try {
+            consumer();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
