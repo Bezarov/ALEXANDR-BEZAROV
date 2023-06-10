d9cb2fb625c564fce6ed877d5883d073acefc015
Alexandr Bezarov <bezarov.sasha@gmail.com>
2023-06-10 13:52:26 +0300

    Homework_14_update

--- src/main/java/homework_nr_15/Main.java
+++ src/main/java/homework_nr_15/Main.java
@@ -1,6 +1,8 @@
 package homework_nr_15;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -15,25 +17,17 @@ public class Main {
         t5.join();
         t6.join();
 
-        ArrayBlockingQueue<Integer> synArray = new ArrayBlockingQueue<>(1, true);
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicBoolean stop = new AtomicBoolean(false);
         new Thread(() -> {
-            for (int i = 0; i < 100000; i++) {
-                try {
-                    synArray.put(i);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            while (count.get() < 1000000 || !stop.get())
+                count.incrementAndGet();
         }).start();
         new Thread(() -> {
-            while (synArray.peek() < 50000) {
-                try {
-                    Thread.sleep(1);
-                    System.out.println(synArray.take() + " " + Thread.currentThread().getName());
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            while (count.get() < 500000)
+                System.out.println(count.get() + " " + Thread.currentThread().getName());
+            stop.set(true);
+            System.out.println("The number reached " + count.get() + " I stop the incrementials " + Thread.currentThread().getName());
         }).start();
     }
 }
